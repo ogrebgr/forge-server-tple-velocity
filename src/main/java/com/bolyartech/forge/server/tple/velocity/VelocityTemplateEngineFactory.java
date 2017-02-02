4df24f73f70068b16d293fec8277a7e7c64dbb4b
Ognyan Bankov <ogibankov@gmail.com>
2017-02-02 08:43:51 +0200

    adding additional settings

--- src/main/java/com/bolyartech/forge/server/tple/velocity/VelocityTemplateEngineFactory.java
+++ src/main/java/com/bolyartech/forge/server/tple/velocity/VelocityTemplateEngineFactory.java
@@ -4,6 +4,8 @@ import com.bolyartech.forge.server.misc.TemplateEngine;
 import com.bolyartech.forge.server.misc.TemplateEngineFactory;
 import org.apache.velocity.app.VelocityEngine;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 
@@ -12,16 +14,26 @@ public class VelocityTemplateEngineFactory implements TemplateEngineFactory {
     private final String mTemplatePathPrefix;
 
 
-    public VelocityTemplateEngineFactory(String templatePathPrefix) {
+    public VelocityTemplateEngineFactory(String templatePathPrefix, Map<String, String> additionalSettings) {
         mVelocityEngine = new VelocityEngine();
         Properties properties = new Properties();
         properties.setProperty("resource.loader", "class");
         properties.setProperty(
                 "class.resource.loader.class",
                 "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
+
+        for(String key : additionalSettings.keySet()) {
+            properties.setProperty(key, additionalSettings.get(key));
+        }
+
         mVelocityEngine.init(properties);
 
         mTemplatePathPrefix = templatePathPrefix;
+    }
+
+
+    public VelocityTemplateEngineFactory(String templatePathPrefix) {
+        this(templatePathPrefix, new HashMap<>());
     }
 
 
